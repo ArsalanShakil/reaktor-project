d857559228fccbd470cba0a43c528b0226a42828
Arsalan <arsalan_cr7@yahoo.com>
2021-01-21 00:01:06 +0500

    network error bug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function fetchBeaniesData() {
+const fetchBeaniesData = async () => {
 
-    fetch('https://cors-anywhere.herokuapp.com/https://bad-api-assignment.reaktor.com/v2/products/beanies')
+    await fetch('https://cors-anywhere.herokuapp.com/https://bad-api-assignment.reaktor.com/v2/products/beanies')
         .then(response => {
             console.log(response)
             if (!response.ok) {
@@ -12,7 +12,10 @@ function fetchBeaniesData() {
             console.log(data);
             const html = data.map(product => {
                 const manufacturerData = product.manufacturer;
-                console.log(manufacturerData);
+                setTimeout(() => {
+                    console.log("World!");
+                }, 3000000000);
+                fetchAvailabilityData(manufacturerData);
 
                 return `
                     <div>
@@ -30,11 +33,13 @@ function fetchBeaniesData() {
 }
 
 fetchBeaniesData();
-/*function fetchBeaniesData() {
 
-    fetch('')
+const fetchAvailabilityData = async (manufacturerData) => {
+
+    /*await fetch('https://cors-anywhere.herokuapp.com/https://bad-api-assignment.reaktor.com/v2/availability/' + manufacturerData)
         .then(response => {
             console.log(response)
+            setTimeout(response.ok, 300);
             if (!response.ok) {
                 throw Error("Error");
             }
@@ -42,13 +47,23 @@ fetchBeaniesData();
         })
         .then(data => {
             console.log(data);
-            console.log(html);
-            document.querySelector('#testing-api').insertAdjacentHTML('afterbegin', html);
         })
         .catch(error => {
             console.log(error);
-        });
+        });*/
+
+    try {
+        const res = await fetch('https://cors-anywhere.herokuapp.com/https://bad-api-assignment.reaktor.com/v2/availability/' + manufacturerData);
+        const json = await res.json();
+        console.log(JSON.stringify(res));
+        console.log("FETCH: ", json);
+
+
+    } catch (e) {
+        console.log("not working",e);
+    }
 }
 
-fetchBeaniesData();*/
+
+
 
